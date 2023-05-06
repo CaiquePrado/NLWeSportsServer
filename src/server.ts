c7c5b9f98500ccb2b5af30c52e2b00da1b120d89
CaiquePrado <caiqueprado76@gmail.com>
2023-05-06 18:46:01 -0300

    feat: create tables game and Ad in Prisma

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,25 @@ const PORT = 3333;
 app.listen(PORT, () => {
   console.log("Server is running");
 });
+
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  return res.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+    { id: 4, name: "Anúncio 4" },
+    { id: 5, name: "Anúncio 5" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  return res.json([]);
+});
